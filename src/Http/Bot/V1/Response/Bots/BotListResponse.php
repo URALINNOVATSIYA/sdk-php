a01193dbd69d363002326f5e811605a7099e8667
davihedg <batrackova.d@gmail.com>
2023-07-31 15:22:42 +0600

    rename BotCollection

--- src/Http/Bot/V1/Response/Bots/BotListResponse.php
+++ src/Http/Bot/V1/Response/Bots/BotListResponse.php
@@ -2,12 +2,12 @@
 
 namespace Twin\Sdk\Http\Bot\V1\Response\Bots;
 
-use Twin\Sdk\Http\Bot\V1\Response\Entity\Bots\BotList\BotListCollection;
+use Twin\Sdk\Http\Bot\V1\Response\Entity\Bots\BotList\BotCollection;
 use Twin\Sdk\Http\Response;
 
 class BotListResponse extends Response
 {
-    public ?BotListCollection $body;
+    public ?BotCollection $body;
 
-    protected string $castBodyTo = '?' . BotListCollection::class;
+    protected string $castBodyTo = '?' . BotCollection::class;
 }
--- src/Http/Bot/V1/Response/Entity/Bots/BotList/BotCollection.php
+++ src/Http/Bot/V1/Response/Entity/Bots/BotList/BotCollection.php
@@ -4,7 +4,7 @@ namespace Twin\Sdk\Http\Bot\V1\Response\Entity\Bots\BotList;
 
 use Twin\Sdk\Entity;
 
-class BotListCollection extends Entity
+class BotCollection extends Entity
 {
     public int $count;
     public BotList $items;
