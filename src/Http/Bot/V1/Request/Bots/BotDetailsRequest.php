fcf47a516e247fdd2035576e80bb4be571b787ba
davihedg <batrackova.d@gmail.com>
2023-07-31 15:23:41 +0600

    fix bot detail request extend and properties

--- src/Http/Bot/V1/Request/Bots/BotDetailsRequest.php
+++ src/Http/Bot/V1/Request/Bots/BotDetailsRequest.php
@@ -2,12 +2,16 @@
 
 namespace Twin\Sdk\Http\Bot\V1\Request\Bots;
 
-use Twin\Sdk\Http\Request\QueryRequest;
+use Twin\Sdk\Http\Request;
 
-class BotDetailsRequest extends QueryRequest
+class BotDetailsRequest extends Request
 {
+    public ?string $fields = null;
+
     public function __construct(array $properties = [])
     {
-        parent::__construct($properties, []);
+        parent::__construct($properties, [
+            'fields'
+        ]);
     }
 }
