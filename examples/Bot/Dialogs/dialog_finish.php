d4695d45d3010a5f46583df1c0d6920ae48ab5d5
davihedg <batrackova.d@gmail.com>
2023-08-04 13:43:46 +0600

    test value cleaned

--- examples/Bot/Dialogs/dialog_finish.php
+++ examples/Bot/Dialogs/dialog_finish.php
@@ -6,9 +6,7 @@ $authenticator = require_once __DIR__ . '/../../authentication.php';
 
 $bot = new BotHttpClient($authenticator);
 
-$bot->testOn();
-
-$dialogId = '0dcd0bd9-300d-4796-a817-2ff46cb079ae';
+$dialogId = '';
 
 $response = $bot->finishDialog($dialogId);
 
